6c4691c0401954840cdaec7c419801016a25217e
stefano.pastore <step848@gmail.com>
2016-11-05 19:17:09 +0100

    Move middleware.

--- src/middleware.js
+++ src/middleware.js
@@ -4,9 +4,17 @@ import {
   asyncAddActionActionCreator,
   asyncPasrsedActionActionCreator,
 } from './actions';
-import { inExecution, actions } from './selectors';
+import { inExecution, actions, parsed } from './selectors';
 
 export default ({ dispatch, getState }) => next => action => {
+  if (
+    typeof action === 'function' &&
+    typeof action.asyncName !== 'undefined' &&
+    parsed(getState()).indexOf(action.asyncName) !== -1
+  ) {
+    return null;
+  }
+
   if (typeof action === 'function' && typeof action.asyncName !== 'undefined') {
     dispatch(asyncAddActionActionCreator(action.asyncName));
 
@@ -14,9 +22,11 @@ export default ({ dispatch, getState }) => next => action => {
   } else if (typeof action.asyncName !== 'undefined') {
     dispatch(asyncPasrsedActionActionCreator(action.asyncName));
 
-    if (actions(getState()).length === 0) {
-      dispatch(asyncEndActionCreator());
-    }
+    setTimeout(() => {
+      if (actions(getState()).length === 0) {
+        dispatch(asyncEndActionCreator());
+      }
+    }, 0);
   }
 
   return next(action);
