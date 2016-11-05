7a831eb4ac66198582a238f82da14f2080ec83b7
stefano.pastore <step848@gmail.com>
2016-11-05 13:47:48 +0100

    Add middlewares for async render

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export reduxUniversalRender from './server/render';
+export reduxUniversalRenderServer from './server/middleware';
+export reduxUniversalRenderClient from './client/middleware';
+export reduxUniversalRenderReducer from './reducer';
+export reduxUniversalRenderActions from './actions';
+export reduxUniversalRenderSelectors from './selectors';
